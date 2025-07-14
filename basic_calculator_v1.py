0f07dd7e86f4bf81852c8b3eb22c62ab28b5eb98
dabaq01 <timothydabak@gmail.com>
2025-07-14 15:34:05 +0100

    basic calculator files for edit

---
+++ basic_calculator_v1.py
@@ -0,0 +1,6 @@
+#!/bin/bash
+#creating a basic calculator
+value1 = 7
+value2 = 10
+sum =value1 + value2
+print (sum)
---
+++ basic_calculator_v2.py
@@ -0,0 +1,5 @@
+#creating a basic calculator
+value1 = input(f"Please enter first number\n>>>")
+value2 = input(f"Please enter second number\n>>>")
+sum = value1 + value2
+print (sum)
